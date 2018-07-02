b1a5b829e9acba1ec6d22f13bc9b1566291250c0
Michael Vinson <MVinsonCPNB@gmail.com>
2018-07-02 19:54:19 -0400

    added file save capabilites to BeatBox.java

--- beatbox/BeatBox.java
+++ beatbox/BeatBox.java
@@ -126,6 +126,50 @@ public class BeatBox {
         }catch(Exception e){e.printStackTrace();}
     }// close buildTrackAndStart
 
+    public class MySendListener implements ActionListener{
+        public void actionPerformed(ActionEvent a){
+            
+            boolean[] checkboxState = new boolean[256];
+
+            for (int i = 0; i < 256; i++){
+                JCheckBox check = (JCheckBox) checkboxList.get(i);
+                if (check.isSelected()){
+                    checkboxState[i] = true;
+                }
+            }
+
+            try{
+                FileOutputStream fileStream = new FileOutputStream(new File("Checkbox.ser"));
+                ObjectOutputStream os = new ObjectOutputStream(fileStream);
+                os.writeObject(checkboxState);
+            }catch(Exception ex){ex.printStackTrace();}
+        }// close actionPerformed() method        
+    } // close MySendListener class
+
+    public class MyReadListener implements ActionListener{
+        
+        public void actionPerformed(ActionEvent a){
+            boolean[] checkboxState = null;
+            try{
+                FileInputStream fileIn = new FileInputStream(new File("Checkbox.ser"));
+                ObjectInputStream is = new ObjectInputStream(fileIn);
+                checkboxState = (boolean[]) is.readObject();
+            }catch(Exception ex){ ex.printStackTrace();}
+
+            for (int i = 0; i < 256; i++){
+                JCheckBox check = (JCheckBox) checkboxList.get(i);
+                if (checkboxState[i]){
+                    check.setSelected(true);
+                }else{
+                    check.setSelected(false);
+                }
+            }
+
+            sequencer.stop();
+            buildTrackAndStart();
+        } // close actionPerformed() method
+    } // close MyReadListener class
+
     public class MyStartListener implements ActionListener{
         public void actionPerformed(ActionEvent a){
             buildTrackAndStart();
